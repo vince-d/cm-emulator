13c59861064d382972358b9a628307374a5dd18e
vince-d <vincent.diener@gmail.com>
2015-01-20 12:11:46 +0100

    Changes to PP shader.

--- shaders/post_processing.fs
+++ shaders/post_processing.fs
@@ -19,6 +19,10 @@ uniform int tex_height;
 uniform float ratio_x;
 uniform float ratio_y;
 
+vec2 rOffset = vec2(0.005, 0.005);
+vec2 gOffset = vec2(0.002, 0.002);
+vec2 bOffset = vec2(0.003, 0.003);
+
 void pp_funct_0(vec2 uv) {
 	// Calculate inverse of color. Set alpha to 1.
     vec4 color = texture2D(tex0, uv);		
@@ -36,6 +40,60 @@ void pp_funct_1(vec2 uv) {
 	//color.g += cos((float(time + 55) + uv.x * 24.9 * uv.y * 10.9) / 32.0) / 3.0;	
 	//color.b += cos((float(time + 23) + uv.x * 77.9 * uv.y * 13.9) / 88.0) / 2.0;	
     gl_FragColor = vec4(color.rgb, 1.0);
+} 
+
+// Chromatic aberration
+void pp_funct_2(vec2 uv) {	
+	float timeMult = (sin(float(time) / 30.0) + 1.0) / 2.0;
+
+	vec2 toMiddle = uv - vec2(0.5 * ratio_x, 0.5 * ratio_y);
+	float ml = pow(length(toMiddle), timeMult);
+	vec2 uv2 = toMiddle * pow(1.005, timeMult * 4.0) + vec2(0.5 * ratio_x, 0.5 * ratio_y);
+	vec2 uv3 = toMiddle * pow(0.995, timeMult * 2.0) + vec2(0.5 * ratio_x, 0.5 * ratio_y);
+	vec2 uv4 = toMiddle * pow(1.006, timeMult * 3.0) + vec2(0.5 * ratio_x, 0.5 * ratio_y);
+
+    vec4 rValue = texture2D(tex0, uv2);  
+    vec4 gValue = texture2D(tex0, uv3);
+    vec4 bValue = texture2D(tex0, uv4);  
+
+	vec2 a = vec2(0.005, 0.01);
+	vec2 b = vec2(0.00, 0.00);
+	vec2 c = vec2(-0.005, 0.005);
+	vec2 d = vec2(0.005, -0.005);
+	
+	rValue += texture2D(tex0, uv2 + uv2 * ml * a);
+	rValue += texture2D(tex0, uv2 + uv2 * ml * b);
+	rValue += texture2D(tex0, uv2 + uv2 * ml * c);
+	rValue += texture2D(tex0, uv2 + uv2 * ml * d);
+	
+	gValue += texture2D(tex0, uv3 + uv3 * ml * a);
+	gValue += texture2D(tex0, uv3 + uv3 * ml * b);
+	gValue += texture2D(tex0, uv3 + uv3 * ml * c);
+	gValue += texture2D(tex0, uv3 + uv3 * ml * d);
+	
+	bValue += texture2D(tex0, uv4 + uv4 * ml * a);
+	bValue += texture2D(tex0, uv4 + uv4 * ml * b);
+	bValue += texture2D(tex0, uv4 + uv4 * ml * c);
+	bValue += texture2D(tex0, uv4 + uv4 * ml * d);
+	
+	gl_FragColor = vec4(rValue.r / 5.0, gValue.g / 5.0, bValue.b / 5.0, 1.0);
+
+} 
+
+// Zoom
+void pp_funct_3(vec2 uv) {	
+	vec2 distToMiddle = (uv - vec2(0.5 * ratio_x, 0.5 * ratio_y));
+	
+	float timeMult = (sin(float(time) / 49.0) + 1.0) * 45.4;
+	distToMiddle *= timeMult;
+	//float a = (distToMiddle) * 10.6;
+    vec4 rValue = texture2D(tex0, uv - rOffset + distToMiddle);  
+    vec4 gValue = texture2D(tex0, uv - gOffset + distToMiddle);
+    vec4 bValue = texture2D(tex0, uv - bOffset + distToMiddle);  
+
+
+	gl_FragColor = vec4(rValue.r, gValue.g, bValue.b, 1.0);
+
 } 
 
 void main()
